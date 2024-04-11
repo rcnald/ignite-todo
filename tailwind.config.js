95f3017588e97082327a8e7d1e136b9bbb9bbc0b
Ronaldo <ronaldomjunior05@gmail.com>
2024-04-11 18:17:39 -0300

    chore: improve development environment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          100: '#F2F2F2',
+          200: '#D9D9D9',
+          300: '#808080',
+          400: '#333333',
+          500: '#262626',
+          600: '#1A1A1A',
+          700: '#0D0D0D',
+        },
+        danger: '#E25858',
+        blue: {
+          default: '#4EA8DE',
+          dark: '#1E6F9F',
+        },
+        purple: {
+          default: '#8284FA',
+          dark: '#5E60CE',
+        },
+      },
+      fontFamily: {
+        inter: '"Inter", sans-serif',
+      },
+    },
   },
   plugins: [],
 }
